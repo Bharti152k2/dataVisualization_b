c2d40525471afeb997e037e375536d31a5bbe455
Bharti152k2 <nidhi15sak@gmail.com>
2025-08-31 22:24:03 +0530

    db code correction

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 function connectDb() {
   mongoose.connect(
-    "mongodb+srv://nidhi15sak_db_user:Qdn7QzYGO4vEzTTK@cluster0.8zuevhx.mongodb.net/DataVisualizationDashboard",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    "mongodb+srv://nidhi15sak_db_user:Qdn7QzYGO4vEzTTK@cluster0.8zuevhx.mongodb.net/DataVisualizationDashboard"
   );
 }
 module.exports = connectDb;
